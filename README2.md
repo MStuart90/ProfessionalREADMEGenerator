0cdc9a433f6c49e8efeb5884d48075d30f5ddac6
Melanie Stuart <mmooradian90@yahoo.com>
2020-11-01 22:13:49 -0500

    pushing badges to README.

---
+++ README2.md
@@ -0,0 +1,22 @@
+
+	lakg
+
+
+
+
+Badges: 
+
+MIT
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+APACHE 2.0
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+
+GNU GPL v3
+
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+
+BSD 3-Clause License
+
+[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)
--- index.js
+++ index.js
@@ -23,27 +23,70 @@ const promptUser = () => {
       message: 'Please write a short description of your project'
     },
     {
+      type: 'list',
       name: 'license',
-      message: 'What kind of license should your project have? (use arrow keys)>MiT
-      APACHE 2.0
-      GPL 3.0 
-      BSD 3
-      NONE'
+			message: 'What kind of license should your project have? (use arrow keys)',
+			choices: ['MIT',
+      'APACHE 2.0',
+      'GNU GPL v3',
+      'BSD 3-Clause License',
+      'NONE']
     },
     {
       name: 'command',
-      message: 'What command should be run to install dependencies? (npm i) or npm install'
-    }
+      message: 'What command should be run to install dependencies? (npm i) or npm install',
+    },
     {
-      name: 'Need to know',
-      message: 'what does the user need to know about the repo? npm install then run node index.js'
-    }
+      name: 'Need-to-know',
+      message: 'what does the user need to know about the repo? npm install then run node index.js',
+    },
     {
         name: 'contributing',
-        message: 'what does the user need to know about contributing to the repo? open a pull request and we'll review it.'
-      }
+        message: 'what does the user need to know about contributing to the repo? open a pull request and we will review it.',
+      },
      
   ]);
 };
 
+const generateREADME = (answers) => {
+	return `
+	# ${answers.github}
+`;
+};
 
+promptUser()
+  .then(answers => {
+		const README = generateREADME(answers);
+		return writeFileAsync('README2.md', README);
+  })
+  .then(() => {
+    console.log('Successfully wrote to index.html');
+  })
+	.catch(err => console.log(err));
+	
+
+
+	// promptUser()
+  // .then(answers => {
+  //   const html = generateHTML(answers);
+
+  //   return writeFileAsync('index.html', html);
+  // })
+  // .then(() => {
+  //   console.log('Successfully wrote to index.html');
+  // })
+  // .catch(err => console.log(err));
+
+
+
+	// var fs = require("fs");
+
+	// fs.writeFile("README.md", answers, function(err) {
+	
+	// 	if (err) {
+	// 		return console.log(err);
+	// 	}
+	
+	// 	console.log("Success!");
+	
+	// });
